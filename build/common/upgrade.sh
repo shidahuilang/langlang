86e7552dd7f8db9dbd260410f31dfffcb58bd3db
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2022-03-23 13:55:12 +0800

    Update upgrade.sh

--- build/common/upgrade.sh
+++ build/common/upgrade.sh
@@ -19,11 +19,11 @@ GET_TARGET_INFO() {
 			export Legacy_Firmware="openwrt-x86-64-generic-squashfs-combined.${Firmware_sfxo}"
 			export UEFI_Firmware="openwrt-x86-64-generic-squashfs-combined-efi.${Firmware_sfxo}"
 			export Firmware_sfx="${Firmware_sfxo}"
-		elif[[ "${TARGET_PROFILE}" == "friendlyarm_nanopi-r4s" ]]; then
+		elif [[ "${TARGET_PROFILE}" == "friendlyarm_nanopi-r4s" ]]; then
 			export Legacy_Firmware="openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.${Firmware_sfxo}"
 			export UEFI_Firmware="openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-squashfs-sysupgrade.${Firmware_sfxo}"
 			export Firmware_sfx="${Firmware_sfxo}"
-		elif[[ "${TARGET_PROFILE}" == "friendlyarm_nanopi-r2s" ]]; then
+		elif [[ "${TARGET_PROFILE}" == "friendlyarm_nanopi-r2s" ]]; then
 			export Legacy_Firmware="openwrt-rockchip-armv8-friendlyarm_nanopi-r2s-ext4-sysupgrade.${Firmware_sfxo}"
 			export UEFI_Firmware="openwrt-rockchip-armv8-friendlyarm_nanopi-r2s-squashfs-sysupgrade.${Firmware_sfxo}"
 			export Firmware_sfx="${Firmware_sfxo}"	
