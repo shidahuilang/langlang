f0f5e6c8b581d0c0de206bc2d170a1cbb1571896
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2023-07-28 18:26:59 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -87,7 +87,7 @@ sed -i 's/"管理权"/"改密码"/g' `egrep "管理权" -rl ./`
 sed -i 's/"带宽监控"/"监控"/g' `egrep "带宽监控" -rl ./`
 sed -i 's/"设置向导"/"向导"/g' `egrep "设置向导" -rl ./`
 
-git clone -b openclash --depth 1 https://github.com/shidahuilang/openwrt-package package/luci-app-openclash 
+
 
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
 cat >"$CLEAR_PATH" <<-EOF
